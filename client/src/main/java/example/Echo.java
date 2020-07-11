d4db35cc6a4f232d31205832768de647762d4c1c
rocky <rocky.tong@soyotec.com>
2020-07-11 21:48:08 +0800

    发了一个设备通知信息

--- client/src/main/java/example/Echo.java
+++ client/src/main/java/example/Echo.java
@@ -21,53 +21,74 @@
 
 package example;
 
-import com.github.devicehive.client.model.CommandFilter;
-import com.github.devicehive.client.model.DHResponse;
-import com.github.devicehive.client.model.DeviceCommandsCallback;
-import com.github.devicehive.client.model.FailureData;
+import cn.hutool.core.util.RandomUtil;
+import com.github.devicehive.client.model.*;
 import com.github.devicehive.client.service.Device;
-import com.github.devicehive.client.service.DeviceCommand;
+
 import com.github.devicehive.client.service.DeviceHive;
+
 
 import org.joda.time.DateTime;
 
-import java.util.Collections;
+import java.util.ArrayList;
+
 import java.util.List;
-import java.util.UUID;
+
 
 public class Echo {
     //DeviceHive settings
-    private static final String accessToken = "";
-    private static final String refreshToken = "";
-
-    private static final String URL = "";
-
+    private static final String accessToken = "eyJhbGciOiJIUzI1NiJ9.eyJwYXlsb2FkIjp7ImEiOlswXSwiZSI6MTU5NDQ0NzAzNjg2MSwidCI6MSwidSI6MSwibiI6WyIqIl0sImR0IjpbIioiXX19.0ByPlY-SV7FQu33ldMx2DiZgiVnfQuRpVrVSZS8Teyc";
+    private static final String refreshToken = "eyJhbGciOiJIUzI1NiJ9.eyJwYXlsb2FkIjp7ImEiOlswXSwiZSI6MTYxMDE3MDAzNjg2MiwidCI6MCwidSI6MSwibiI6WyIqIl0sImR0IjpbIioiXX19.8msfHfdRL8KoNb8n0ii8qzmwEMOwtwuvI4hnaB4hrss";
+    private static final String URL = "http://localhost/api/rest";
     private static final String COMMAND_NAME = "echo_command";
 
     public static void main(String[] args) {
         final DeviceHive deviceHive = DeviceHive.getInstance().init(URL, refreshToken, accessToken);
-        final String deviceId = UUID.randomUUID().toString();
-        DHResponse<Device> deviceResponse = deviceHive.getDevice(deviceId);
+        final String deviceId = "IOuDBdHnTT7gFCDdHiaKm1BfecZG2JDIPHSE";
+
+        final DHResponse<Device> deviceResponse = deviceHive.getDevice(deviceId);
         if (!deviceResponse.isSuccessful()) {
             System.out.println(deviceResponse);
             return;
         }
-        Device device = deviceResponse.getData();
-        CommandFilter filter = getFilter();
-        deviceHive.subscribeCommands(Collections.singletonList(deviceId), filter, new DeviceCommandsCallback() {
+        final Device device = deviceResponse.getData();
+        new Thread(new Runnable() {
             @Override
-            public void onSuccess(List<DeviceCommand> commands) {
-                System.out.println("echo");
-                deviceHive.removeDevice(deviceId);
-                Runtime.getRuntime().exit(200);
-            }
+            public void run() {
+                while(true) {
+                    List<Parameter> parameters = new ArrayList<>();
+                    double t=25;
+                    t=t+ RandomUtil.randomDouble(-10,15);
 
-            @Override
-            public void onFail(FailureData failureData) {
-                System.out.println(failureData);
+                    Parameter parameter = new Parameter("temp", ""+t);
+                    parameters.add(parameter);
+                    DHResponse<DeviceNotification>  r=    device.sendNotification("info", parameters);
+                    System.out.println("========send result:"+r.getData());
+                    try {
+                        Thread.sleep(10000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
             }
-        });
-        device.sendCommand(COMMAND_NAME, null);
+        }).start();
+
+
+//        CommandFilter filter = getFilter();
+//        deviceHive.subscribeCommands(Collections.singletonList(deviceId), filter, new DeviceCommandsCallback() {
+//            @Override
+//            public void onSuccess(List<DeviceCommand> commands) {
+//                System.out.println("echo");
+//                deviceHive.removeDevice(deviceId);
+//                Runtime.getRuntime().exit(200);
+//            }
+//
+//            @Override
+//            public void onFail(FailureData failureData) {
+//                System.out.println(failureData);
+//            }
+//        });
+//        device.sendCommand(COMMAND_NAME, null);
     }
 
     private static CommandFilter getFilter() {
